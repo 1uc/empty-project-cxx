cc1a59c08054fce0355d34354d98e25ff03c068e
Luc Grosheintz <luc.grosheintz@gmail.com>
2018-04-19 16:41:11 +0200

    Add support for clang sanitizers.

--- test/demo.cpp
+++ test/demo.cpp
@@ -1,5 +1,14 @@
 #include "catch/catch.hpp"
 
-TEST_CASE("Trivial demo to test build system.") {
-  REQUIRE(1 == 1);
+TEST_CASE("Simple assert.") { REQUIRE(1 == 1); }
+
+TEST_CASE("Use after free.", "[.][use-after-free]") {
+  double *p = (double *)malloc(10 * sizeof(double));
+  free(p);
+  REQUIRE(p[4] == 42);
+}
+
+TEST_CASE("Out-of-bounds access.", "[.][out-of-bounds]") {
+  double p[] = {42, 42, 42, 42};
+  REQUIRE(p[4] == 42);
 }
